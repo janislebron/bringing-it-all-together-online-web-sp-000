814a35d703dc5e6295ffdc984f3088bc188e0fca
Janis Lebron <janislebron@gmail.com>
2021-04-11 22:49:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,7 @@ class Dog
     SQL
 
     row = DB[:conn].execute(sql, id)[0]
-    Dog.new(result [0], result [1], result [2])
+    Dog.new(result[0], result[1], result[2])
   end
 
   def self.create(name:, breed:)
